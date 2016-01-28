1c62db1ea9e3e16253227b96ca3493788f6c3676
Ertunç Efeoğlu <ertuncefeoglu@gmail.com>
2016-01-28 18:15:10 +0200

    JavaScript design patters tutorials
    Tutorial code for JavaScript design patterns

---
+++ js/factory/image.js
@@ -0,0 +1,12 @@
+﻿define(function () {
+    'use strict';
+
+    var Image = function (attributes) {
+        this.width = attributes.width || 0;
+        this.width = attributes.heigth || 0;
+        this.name = attributes.name || '';
+
+    }
+
+    return Image;
+})
